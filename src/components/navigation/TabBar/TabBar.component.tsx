762a74cb49a338d518512ed9cecbdbe67be22d37
msobczyk-x <m.sobczyk55@wp.pl>
2025-05-04 01:29:31 +0200

    Linter

--- src/components/navigation/TabBar/TabBar.component.tsx
+++ src/components/navigation/TabBar/TabBar.component.tsx
@@ -1,55 +1,55 @@
-import React from 'react';
-import {View, TouchableOpacity} from 'react-native';
-import {useSafeAreaInsets} from 'react-native-safe-area-context';
 import {Typography} from '@/components/ui/Typography';
+import React from 'react';
+import {TouchableOpacity, View} from 'react-native';
+import {useSafeAreaInsets} from 'react-native-safe-area-context';
 import {styles} from './TabBar.styled';
 
 type TabBarProps = {
-  navigation: any;
-  state: any;
-  descriptors: {[key: string]: any};
+	navigation: any;
+	state: any;
+	descriptors: {[key: string]: any};
 };
 
 const TabBar = ({state, descriptors, navigation}: TabBarProps) => {
-  const insets = useSafeAreaInsets();
-  return (
-    <View style={[styles.container, {paddingBottom: insets.bottom}]}>
-      {state.routes.map((route: any, index: any) => {
-        const {options} = descriptors[route.key];
-        const label = options.tabBarLabel || options.title || route.name;
-        const isFocused = state.index === index;
-        const icon = options.tabBarIcon;
+	const insets = useSafeAreaInsets();
+	return (
+		<View style={[styles.container, {paddingBottom: insets.bottom}]}>
+			{state.routes.map((route: any, index: any) => {
+				const {options} = descriptors[route.key];
+				const label = options.tabBarLabel || options.title || route.name;
+				const isFocused = state.index === index;
+				const icon = options.tabBarIcon;
 
-        const onPress = () => {
-          const event = navigation.emit({
-            type: 'tabPress',
-            target: route.key,
-            canPreventDefault: true,
-          });
+				const onPress = () => {
+					const event = navigation.emit({
+						type: 'tabPress',
+						target: route.key,
+						canPreventDefault: true,
+					});
 
-          if (!isFocused && !event.defaultPrevented) {
-            navigation.navigate(route.name);
-          }
-        };
+					if (!isFocused && !event.defaultPrevented) {
+						navigation.navigate(route.name);
+					}
+				};
 
-        return (
-          <TouchableOpacity
-            key={index}
-            accessibilityRole="button"
-            accessibilityState={isFocused ? {selected: true} : {}}
-            accessibilityLabel={options.tabBarAccessibilityLabel}
-            testID={options.tabBarTestID}
-            onPress={onPress}
-            style={[styles.tabItem, isFocused && styles.activeTabItem]}>
-            {icon ? icon() : null}
-            <Typography variant="buttonText" style={styles.tabLabel}>
-              {label}
-            </Typography>
-          </TouchableOpacity>
-        );
-      })}
-    </View>
-  );
+				return (
+					<TouchableOpacity
+						key={index}
+						accessibilityRole="button"
+						accessibilityState={isFocused ? {selected: true} : {}}
+						accessibilityLabel={options.tabBarAccessibilityLabel}
+						testID={options.tabBarTestID}
+						onPress={onPress}
+						style={[styles.tabItem, isFocused && styles.activeTabItem]}>
+						{icon ? icon() : null}
+						<Typography variant="buttonText" style={styles.tabLabel}>
+							{label}
+						</Typography>
+					</TouchableOpacity>
+				);
+			})}
+		</View>
+	);
 };
 
 export default TabBar;
--- src/stacks/TabNavigation/screens/CharacterList/CharacterList.screen.tsx
+++ src/stacks/TabNavigation/screens/CharacterList/CharacterList.screen.tsx
@@ -6,88 +6,88 @@ import {CharacterListItem} from '@/stacks/TabNavigation/screens/components/Chara
 import {searchQueryAtom} from '@/stores/filters';
 import {useNavigation} from '@react-navigation/native';
 import {FlashList} from '@shopify/flash-list';
+import {useAtomValue} from 'jotai';
 import React, {useCallback, useMemo} from 'react';
 import {ActivityIndicator, View} from 'react-native';
 import type {MainStackNavigationProp} from '../../../Main/Main.routes';
 import {CharacterListHeader} from '../components/CharacterListHeader';
 import {styles} from './CharacterList.styled';
-import {useAtomValue} from 'jotai';
 
 const CharacterListScreen = () => {
-  const searchQuery = useAtomValue(searchQueryAtom);
-  const {navigate} = useNavigation<MainStackNavigationProp>();
-  const {
-    data,
-    isLoading,
-    isRefetching,
-    refetch,
-    hasNextPage,
-    isFetchingNextPage,
-    fetchNextPage,
-  } = useCharactersList(httpClient, {
-    name: searchQuery,
-  });
+	const searchQuery = useAtomValue(searchQueryAtom);
+	const {navigate} = useNavigation<MainStackNavigationProp>();
+	const {
+		data,
+		isLoading,
+		isRefetching,
+		refetch,
+		hasNextPage,
+		isFetchingNextPage,
+		fetchNextPage,
+	} = useCharactersList(httpClient, {
+		name: searchQuery,
+	});
 
-  const listData = useMemo(
-    () => data?.pages.flatMap(data => data.results) || [],
-    [data],
-  );
-  const handleLoadMore = useCallback(() => {
-    if (hasNextPage && !isFetchingNextPage) {
-      fetchNextPage();
-    }
-  }, [hasNextPage, isFetchingNextPage, fetchNextPage]);
+	const listData = useMemo(
+		() => data?.pages.flatMap(data => data.results) || [],
+		[data],
+	);
+	const handleLoadMore = useCallback(() => {
+		if (hasNextPage && !isFetchingNextPage) {
+			fetchNextPage();
+		}
+	}, [hasNextPage, isFetchingNextPage, fetchNextPage]);
 
-  return (
-    <View style={styles.container}>
-      <View style={styles.header}>
-        <CharacterListHeader />
-      </View>
-      <FlashList
-        data={listData}
-        estimatedItemSize={224}
-        contentContainerStyle={styles.listContainer}
-        showsVerticalScrollIndicator={false}
-        onEndReached={handleLoadMore}
-        onEndReachedThreshold={0.5}
-        ItemSeparatorComponent={() => <View style={styles.listDivider} />}
-        onRefresh={() => refetch()}
-        refreshing={isRefetching}
-        ListFooterComponent={() => {
-          if (isFetchingNextPage) {
-            return (
-              <ActivityIndicator size="small" color={theme.colors.darkGreen} />
-            );
-          }
-          return null;
-        }}
-        ListEmptyComponent={() =>
-          isLoading ? (
-            <ActivityIndicator size={'large'} />
-          ) : (
-            <View style={styles.emptyListContainer}>
-              <Typography variant="bodyText">No characters found</Typography>
-            </View>
-          )
-        }
-        renderItem={({item}) => (
-          <CharacterListItem
-            name={item.name}
-            characterId={item.id}
-            status={item.status}
-            image={item.image}
-            species={item.species}
-            onPress={() =>
-              navigate('CharacterDetailsStack', {
-                screen: 'CharacterDetailsScreen',
-                params: {characterId: item.id},
-              })
-            }
-          />
-        )}
-      />
-    </View>
-  );
+	return (
+		<View style={styles.container}>
+			<View style={styles.header}>
+				<CharacterListHeader />
+			</View>
+			<FlashList
+				data={listData}
+				estimatedItemSize={224}
+				contentContainerStyle={styles.listContainer}
+				showsVerticalScrollIndicator={false}
+				onEndReached={handleLoadMore}
+				onEndReachedThreshold={0.5}
+				ItemSeparatorComponent={() => <View style={styles.listDivider} />}
+				onRefresh={() => refetch()}
+				refreshing={isRefetching}
+				ListFooterComponent={() => {
+					if (isFetchingNextPage) {
+						return (
+							<ActivityIndicator size="small" color={theme.colors.darkGreen} />
+						);
+					}
+					return null;
+				}}
+				ListEmptyComponent={() =>
+					isLoading ? (
+						<ActivityIndicator size={'large'} />
+					) : (
+						<View style={styles.emptyListContainer}>
+							<Typography variant="bodyText">No characters found</Typography>
+						</View>
+					)
+				}
+				renderItem={({item}) => (
+					<CharacterListItem
+						name={item.name}
+						characterId={item.id}
+						status={item.status}
+						image={item.image}
+						species={item.species}
+						onPress={() =>
+							navigate('CharacterDetailsStack', {
+								screen: 'CharacterDetailsScreen',
+								params: {characterId: item.id},
+							})
+						}
+					/>
+				)}
+			/>
+		</View>
+	);
 };
 
 export default CharacterListScreen;
