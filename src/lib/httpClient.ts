2a639c17d3220d6ddbbf57b76ad7141b89b0c76f
msobczyk-x <m.sobczyk55@wp.pl>
2025-05-03 19:49:46 +0200

    Linter fixes

--- src/lib/httpClient.ts
+++ src/lib/httpClient.ts
@@ -1,7 +1,7 @@
 import ky from 'ky';
 
 const httpClient = ky.create({
-  prefixUrl: process.env.EXPO_PUBLIC_API_URL,
+	prefixUrl: process.env.EXPO_PUBLIC_API_URL,
 });
 
 export type HttpClient = typeof httpClient;
